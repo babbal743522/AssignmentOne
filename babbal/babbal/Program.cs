c30d042722664d281921bb1e59135d63ace3cb60
Babbalpreet Kaur Bhari <743522@student.tor.loc>
2020-01-20 13:58:32 -0500

    commit 1

---
+++ babbal/babbal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C0743522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double INPUT
+            Console.WriteLine("Hello!");
+        }
+    }
+}
+
