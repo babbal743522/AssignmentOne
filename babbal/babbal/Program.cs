59f5079779dde7293f73c930874c9d061dcd89f0
Babbalpreet Kaur Bhari <743522@student.tor.loc>
2020-01-20 15:17:31 -0500

    commit A-C

--- babbal/babbal/Program.cs
+++ babbal/babbal/Program.cs
@@ -6,22 +6,21 @@ namespace C0743522
     {
        
 
-            public static int Sum(int num1, int num2)
-    {
-        int total;
-        total = num1 + num2;
-        return total;
-    }
+           public static void Main()
+  {
+    int num1, num2;
      
-    public static void Main()
-    {
-	 
-	  Console.Write("Enter a number: ");
-      int n1= Convert.ToInt32(Console.ReadLine());
-      Console.Write("Enter another number: ");
-      int n2= Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(n1,n2) );
-    }
-    }
+    Console.Write("Enter the first number");
+    num1 = Convert.ToInt32(Console.ReadLine());
+     
+    Console.Write("Enter the second number");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    
+     
+    int result = num1 * num2 ;
+    Console.WriteLine("Output: {0} x {1} = {2}", 
+                        num1, num2, result);
+  }
 }
+    }
 
