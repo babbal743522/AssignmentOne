c9cfbf6e1bbf1f2d357a047dc324f4a849debdd6
Babbalpreet Kaur Bhari <743522@student.tor.loc>
2020-01-27 12:35:41 -0500

    commit A-E

--- babbal/babbal/Program.cs
+++ babbal/babbal/Program.cs
@@ -7,20 +7,33 @@ namespace C0743522
   
              public static void Main()
   {
-    int num1, num2, num3;
-     
-    Console.Write("please enter the first number");
-    num1 = Convert.ToInt32(Console.ReadLine());
-     
-    Console.Write("please enter the second number");
-    num2 = Convert.ToInt32(Console.ReadLine());
-     
-    Console.Write("please enter the third number");
-    num3 = Convert.ToInt32(Console.ReadLine());
-     
-    int resultOfMultiplication = num1 * num2 * num3;
-    Console.WriteLine("Output: {0} x {1} x {2} = {3}", 
-                        num1, num2, num3, resultOfMultiplication);
-  }
+    
+  int x;
+  int result;
+ 
+  Console.WriteLine("Enter any  number:");
+  x = Convert.ToInt32(Console.ReadLine() );
+   
+  result = x * 1;
+  Console.WriteLine("The table is : {0} x {1} = {2}", x, 1, result);
+  result = x * 2;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 2, result);
+  result = x * 3;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 3, result);
+  result = x * 4;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 4, result);
+  result = x * 5;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 5, result);
+  result = x * 6;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 6, result);
+  result = x * 7;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 7, result);
+  result = x * 8;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 8, result);
+  result = x * 9;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 9, result);
+  result = x * 10;
+  Console.WriteLine("             : {0} x {1} = {2}", x, 10, result);
+ }
 }
     }
